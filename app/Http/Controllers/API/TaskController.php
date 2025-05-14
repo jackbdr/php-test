4ce2c81928ea9aae122667bb3b9d8adc14a3ef0e
Jack R <jackbdrobinson@gmail.com>
2025-05-14 19:05:33 +0200

    add Task controller

---
+++ app/Http/Controllers/API/TaskController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\TaskRequest;
+use App\Http\Resources\TaskResource;
+use App\Models\Task;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): JsonResponse
+    {
+        $tasks = Task::all();
+
+        return response()->json([
+            'success' => true,
+            'data' => TaskResource::collection($tasks),
+            'message' => 'Tasks retrieved successfully.'
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(TaskRequest $request): JsonResponse
+    {
+        $task = Task::create($request->validated());
+
+        return response()->json([
+            'success' => true,
+            'data' => new TaskResource($task),
+            'message' => 'Task created successfully.'
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Task $task): JsonResponse
+    {
+        return response()->json([
+            'success' => true,
+            'data' => new TaskResource($task),
+            'message' => 'Task retrieved successfully.'
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
